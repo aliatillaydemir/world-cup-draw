da93a6601af4ea6a53238f6ed1966e261f3d4e84
atilla <atilla-734@hotmail.com>
2020-01-21 22:12:06 +0300

    added with git

---
+++ src/main/java/AnaClass.java
@@ -0,0 +1,25 @@
+
+
+public class AnaClass extends Kuramenusu{
+    
+    public static void main(String[] args) {
+        
+    
+        
+    
+        main_screen ana_ekran=new main_screen();
+        ana_ekran.setVisible(true);
+       
+     // Kuramenusu ekran =new Kuramenusu();
+      
+       //  ekran.setVisible(true);
+          
+    }        
+
+   
+
+}
+ 
+    
+
+    
